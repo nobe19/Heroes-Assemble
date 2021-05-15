c3da00e6b371456c0dd7e1f15dacd4f3be09ac4e
Nipuni Obe <obe@gmail.com>
2021-05-15 02:47:32 -0400

    ...

--- Heroes Assemble/Heroes.swift
+++ Heroes Assemble/Heroes.swift
@@ -5,8 +5,20 @@
 //  Created by Nipuni Obe on 5/15/21.
 //
 
-import Foundation
+//import Foundation
 
-class Heroes {
-    
-}
+//class Heroes {
+//
+//    struct Returned: Codable {
+//        var id: [Hero] //what does var name need to be? //id?
+//    }
+//
+//    struct Hero: Codable {
+//        var strID = ""
+//    }
+//
+//    let urlString = "https://superheroapi.com/api/3876294599135188/1"
+//    var heroArray: [Hero] = []
+//
+//
+//}
